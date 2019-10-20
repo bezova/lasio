58a4a8079945588be2154a0982bf08d8e759ef98
Kent Inverarity <kinverarity@hotmail.com>
2019-10-20 17:12:46 +1030

    Remove redundant check

--- lasio/spec.py
+++ lasio/spec.py
@@ -32,15 +32,6 @@ class AsciiSectionExists(Rule):
             return False
 
 
-class MandatorySections(Rule):
-    @staticmethod
-    def check(las_file):
-        return VersionSectionExists.check(las_file) and \
-               WellSectionExists.check(las_file) and \
-               CurvesSectionExists.check(las_file) and \
-               AsciiSectionExists.check(las_file)
-
-
 class MandatoryLinesInVersionSection(Rule):
     @staticmethod
     def check(las_file):
